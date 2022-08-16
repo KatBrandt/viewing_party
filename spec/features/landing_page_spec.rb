17f8a923908653171fc39b379fb6def984342156
Kat Brandt <joyfulkat11@gmail.com>
2022-08-16 13:51:35 -0600

    Create Users Controller.
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -7,5 +7,13 @@ describe "Landing Page" do
 
     expect(page).to have_content("Viewing Party")
     expect(page).to have_button("Create New User")
-  end 
+  end
+
+  it 'can click button to create new user' do
+    visit root_path
+
+    click_on "Create New User"
+
+    expect(current_path).to eq new_user_path
+  end
 end
